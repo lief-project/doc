1bb838caed0666dd3a4ad33f82bab48990355981
Romain Thomas <me@romainthomas.fr>
2020-12-23 07:28:57 +0100

    Update

--- generate_doc.sh
+++ generate_doc.sh
@@ -16,31 +16,18 @@ tar -xvf LIEF-${LIEF_VERSION}-Linux.tar.gz
 curl -LO https://github.com/lief-project/LIEF/archive/master.tar.gz
 tar -xvf master.tar.gz
 
-# Setup LIEF doc assets
-cp LIEF-master/examples/cmake/find_package/CMakeLists.txt     LIEF-master/doc/sphinx/_static/CMakeFindPackage.cmake
-cp LIEF-master/examples/cmake/find_package/README.rst         LIEF-master/doc/sphinx/_static/ReadmeFindPackage.rst
-cp LIEF-master/examples/cmake/external_project/CMakeLists.txt LIEF-master/doc/sphinx/_static/CMakeExternalProject.cmake
-cp LIEF-master/examples/cmake/external_project/README.rst     LIEF-master/doc/sphinx/_static/ReadmeExternalProject.rst
-
 # Doxygen
 # =======================================================================
-
-# TODO(romain): To update when it will be merged in master
-curl -LO https://gist.githubusercontent.com/romainthomas/1ffc98fa20216a09b28baa305af048a4/raw/55b80ef5dfa1ea9a637d2cf48500019d755737ee/Doxyfile
 
 LIEF_INPUT=LIEF-${LIEF_VERSION}-Linux/include/LIEF \
 LIEF_EXCLUDE=LIEF-${LIEF_VERSION}-Linux/include/LIEF/third-party \
 LIEF_INCLUDE_PATH=LIEF-${LIEF_VERSION}-Linux/include \
-doxygen Doxyfile
+doxygen LIEF-master/doc/doxygen/Doxyfile
 
 # Sphinx
 # =======================================================================
 
 cd LIEF-master/doc
-# TODO(romain): To update when it will be merged in master
-curl -LO https://gist.githubusercontent.com/romainthomas/8a503a1f3c13862e1339a8f61e679fbf/raw/3fda36bdcb00b8c28ff70f8386ea07cdb2bb75dc/conf.py
-mv conf.py sphinx/
-
 LIEF_DOXYGEN_XML=$HOME/doxygen/xml/ \
 sphinx-build -a -E -j8 -w sphinx-warn.log ./sphinx ./sphinx-doc
 
